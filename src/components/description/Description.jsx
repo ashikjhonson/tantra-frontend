ede9856ef82ee6819b85b4e50368b1a92dc7c157
Ashik Jhonson <ashikjhonson@gmail.com>
2023-11-18 01:11:54 +0530

    Marquee incmplt

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -3,19 +3,15 @@ import MarqueeComp from "../marquee/MarqueeComp";
 
 const Description = () => {
   return (
-    <div className="bg-zinc-300 pt-10 pb-20">
+    <div className="bg-zinc-300 py-10">
       <hr className="border-gray-500 pb-10" />
-      <ul className="pl-28 mb-20 flex flex-col gap-2 font-semibold font-sans text-7xl text-black">
+      <ul className="pl-28 mb-10 flex flex-col gap-2 font-semibold font-sans text-7xl text-black">
         <li>Connect</li>
         <li>Collaborate</li>
         <li>Catalyze.</li>
       </ul>
-      <div className="rotate-6">
-        <MarqueeComp />
-      </div>
-      <div className="-rotate-12">
-        <MarqueeComp />
-      </div>
+      <MarqueeComp mdir="right" rot="-rotate-6" />
+      <MarqueeComp rot="rotate-6" />
     </div>
   );
 };
--- src/components/marquee/MarqueeComp.jsx
+++ src/components/marquee/MarqueeComp.jsx
@@ -1,17 +1,24 @@
 import Marquee from "react-fast-marquee";
 
-const MarqueeComp = ({ word = "| TANTRA'23 |" }) => {
+const MarqueeComp = ({
+  word = "| TANTRA'23 |",
+  mdir = "left",
+  rot = "rotate-0",
+}) => {
   return (
-    <Marquee
-      loop={0}
-      speed={100}
-      autoFill
-      // gradient
-      // gradientColor="rgba(25, 255, 25, 0.5)"
-      // gradientWidth={800}
-    >
-      <p className="py-4 text-2xl  bg-black">{word}</p>
-    </Marquee>
+    <div className={rot + " w-full"}>
+      <Marquee
+        loop={0}
+        speed={50}
+        autoFill
+        direction={mdir}
+        gradient
+        gradientColor="rgba(255, 255, 255, 0.3)"
+        gradientWidth={800}
+      >
+        <p className="py-4 text-2xl bg-black">{word}</p>
+      </Marquee>
+    </div>
   );
 };
 
