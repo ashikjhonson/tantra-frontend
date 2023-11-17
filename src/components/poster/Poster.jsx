7880c50a3a8d4cf350e3a5f53a8d3361a5b1328b
Ashik Jhonson <ashikjhonson@gmail.com>
2023-11-17 23:51:48 +0530

    Landing

--- src/components/poster/Poster.jsx
+++ src/components/poster/Poster.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 //import IntroImage from '../../assets/intro.jpg';
-import PosterImage from "../../assets/poster.png";
+import PosterImage from "../../assets/bg/desktop.png";
+import PosterImg from "../../assets/bg.png";
+// import PosterImg from "../../assets/poster.png";
 import ParticlesComponent from "../particles/ParticlesComponent";
 import VideoPlayer from "../videoPlayer/VideoPlayer";
 
@@ -9,17 +11,19 @@ import "./Poster.css";
 function Poster() {
   return (
     <div className="hero">
-      {/* <div className="particle_div">
+      <div className="particle_div">
         <ParticlesComponent className="part" />
-      </div> */}
+      </div>
       {/* <div id="tsparticles"></div> */}
       <div className="fill intro_image">
-        <img src={PosterImage} alt="poster" className="intro_image" />
+        <img src={PosterImg} alt="poster" className="intro_image" />
       </div>
       <div className="content">
-        <VideoPlayer />
+        {/* <VideoPlayer /> */}
         <p className="tantra relative z-1">TANTRA"23</p>
-        <p className="eventdate relative z-1">DECEMBER 04, 2023</p>
+        <p className="border-2 rounded-full w-fit mx-auto px-8 py-1 relative z-1">
+          DEC 04
+        </p>
       </div>
     </div>
   );
