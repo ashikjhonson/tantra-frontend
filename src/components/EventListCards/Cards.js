b547541d681204cfbeb27dcb807ce5b07cbf43ba
Vaishnav Krishna <69088196+Appu779@users.noreply.github.com>
2023-11-19 23:25:57 +0530

    some changes with text and size

--- src/components/EventListCards/Cards.js
+++ src/components/EventListCards/Cards.js
@@ -7,23 +7,25 @@ function Cards(props) {
   let navigate = useNavigate();
   let eventData = props.data;
   return (
-    <div className="w-72 p-4 rounded-3xl overflow-hidden shadow-lg bg-red-700">
-      <div className="h-44 w-30 rounded-xl bg-red-200"></div>
+    <div className="w-72 p-4 rounded-3xl overflow-hidden shadow-lg bg-gray-200">
+      <div className="h-44 w-30 rounded-xl bg-red-200">
+      <p className="font-mono text-center">{props.data.event_type}</p>
+        <p className="font-mono text-center">
+          {props.data.fee !== "" ? `Reg Fee: ${props.data.fee}` : "‎ "}
+        </p>
+      </div>
       <div className="pt-5">
         <div className="font-bold font-sans text-3xl mb-2 h-16">
           {props.data.name}
         </div>
-        <p className="font-mono">{props.data.event_type}</p>
-        <p className="font-mono">
-          {props.data.fee !== "" ? `Reg Fee: ${props.data.fee}` : "‎ "}
-        </p>
+       
       </div>
       <div className="px-0 pt-4 pb-2">
         <a>{/* <ViewDetails data={eventData} /> */}</a>
         {eventData.reg ? (
           <a className="">
             <button
-              className="bg-white text-black px-6 py-2 rounded-full"
+              className="align bg-white text-black px-6 py-2 rounded-full"
               onClick={() => navigate("/register", { state: eventData })}
             >
               Register Event
